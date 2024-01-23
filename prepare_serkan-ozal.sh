ecdc6402c353e267ce838010222ffa4ff1b9d90e
Serkan ÖZAL <sozal@catchpoint.com>
2024-01-23 23:21:17 +0300

    graalvm

--- prepare_serkan-ozal.sh
+++ prepare_serkan-ozal.sh
@@ -22,7 +22,7 @@ sdk use java 21.0.2-graal 1>&2
 if [ ! -f target/CalculateAverage_serkan_ozal_image ]; then
     #NATIVE_IMAGE_OPTS="--gc=epsilon -O3 -march=native --enable-preview -H:InlineAllBonus=10 -H:-ParseRuntimeOptions"
     NATIVE_IMAGE_OPTS="--initialize-at-build-time=dev.morling.onebrc.CalculateAverage_serkan_ozal --gc=epsilon -Ob -O3 -march=native --strict-image-heap"
-    NATIVE_IMAGE_OPTS="$NATIVE_IMAGE_OPTS -H:InlineAllBonus=10 -H:-ParseRuntimeOptions --enable-preview -dsa -R:MaxHeapSize=256m"
+    NATIVE_IMAGE_OPTS="$NATIVE_IMAGE_OPTS -H:InlineAllBonus=100 -H:-ParseRuntimeOptions --enable-preview -dsa -R:MaxHeapSize=256m"
     # Use -H:MethodFilter=CalculateAverage_serkan_ozal.* -H:Dump=:2 -H:PrintGraph=Network for IdealGraphVisualizer graph dumping.
 
     native-image $NATIVE_IMAGE_OPTS -cp target/average-1.0.0-SNAPSHOT.jar -o target/CalculateAverage_serkan_ozal_image dev.morling.onebrc.CalculateAverage_serkan_ozal
