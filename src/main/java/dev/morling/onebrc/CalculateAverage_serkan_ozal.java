3e6394e6cc56de64c6c0333e8968fcc3d7c28502
Serkan ÖZAL <sozal@catchpoint.com>
2024-01-25 21:42:21 +0300

    wip

--- src/main/java/dev/morling/onebrc/CalculateAverage_serkan_ozal.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_serkan_ozal.java
@@ -621,13 +621,14 @@ public class CalculateAverage_serkan_ozal {
 //                                NATIVE_BYTE_ORDER);
                 // ByteVector entryKeyVector = ByteVector.fromArray(BYTE_SPECIES, data, keyStartOffset - Unsafe.ARRAY_BYTE_BASE_OFFSET);
                 ByteVector entryKeyVector;
-//                try (Arena arena = Arena.ofConfined()) {
-//                    MemorySegment segment = MemorySegment.ofAddress(entryKeyPtr)
-//                            .reinterpret(BYTE_SPECIES_SIZE, arena, null);
-//                    entryKeyVector = ByteVector.fromMemorySegment(BYTE_SPECIES, segment, 0, NATIVE_BYTE_ORDER);
-//                }
-                MemorySegment segment = MemorySegment.ofAddress(entryKeyPtr).reinterpret(BYTE_SPECIES_SIZE, keyCheckArena, null);
-                entryKeyVector = ByteVector.fromMemorySegment(BYTE_SPECIES, segment, 0, NATIVE_BYTE_ORDER);
+                try (Arena arena = Arena.ofConfined()) {
+                    MemorySegment segment = MemorySegment.ofAddress(entryKeyPtr)
+                            .reinterpret(BYTE_SPECIES_SIZE, arena, null);
+                    entryKeyVector = ByteVector.fromMemorySegment(BYTE_SPECIES, segment, 0, NATIVE_BYTE_ORDER);
+                }
+//                MemorySegment segment = MemorySegment.ofAddress(entryKeyPtr)
+//                        .reinterpret(BYTE_SPECIES_SIZE, keyCheckArena, null);
+//                entryKeyVector = ByteVector.fromMemorySegment(BYTE_SPECIES, segment, 0, NATIVE_BYTE_ORDER);
                 long eqMask = keyVector.compare(VectorOperators.EQ, entryKeyVector).toLong();
                 int eqCount = Long.numberOfTrailingZeros(~eqMask);
                 if (eqCount >= keyCheckLength) {
