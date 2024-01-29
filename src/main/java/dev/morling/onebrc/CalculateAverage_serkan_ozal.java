4e6f28abf972612d1a31a7f39c2c7b8138068de4
Serkan ÖZAL <sozal@catchpoint.com>
2024-01-29 22:20:21 +0300

    wip

--- src/main/java/dev/morling/onebrc/CalculateAverage_serkan_ozal.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_serkan_ozal.java
@@ -648,9 +648,8 @@ public class CalculateAverage_serkan_ozal {
                 // Since majority of the city names >= 8 bytes and <= 16 bytes,
                 // this way is more efficient (according to my experiments) than any other comparisons (byte by byte or 2 longs).
                 ByteVector entryKeyVector = ByteVector.fromArray(BYTE_SPECIES, data, keyStartArrayOffset);
-                // long eqMask = keyVector.compare(VectorOperators.EQ, entryKeyVector).toLong();
-                // int eqCount = Long.numberOfTrailingZeros(~eqMask);
-                int eqCount = keyVector.compare(VectorOperators.EQ, entryKeyVector).not().firstTrue();
+                long eqMask = keyVector.compare(VectorOperators.EQ, entryKeyVector).toLong();
+                int eqCount = Long.numberOfTrailingZeros(~eqMask);
                 if (eqCount >= keyLength) {
                     return true;
                 }
