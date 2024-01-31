ed802a9fb7102df4aafef37883e79436d4cc79fa
Serkan ÖZAL <sozal@catchpoint.com>
2024-01-31 22:13:01 +0300

    wip

--- src/main/java/dev/morling/onebrc/CalculateAverage_serkan_ozal.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_serkan_ozal.java
@@ -488,7 +488,7 @@ public class CalculateAverage_serkan_ozal {
                 ByteVector entryKeyVector2 = ByteVector.fromArray(BYTE_SPECIES, map.data, entryOffset2 + OpenMap.KEY_ARRAY_OFFSET);
 
                 int eqCount1 = keyVector1.compare(VectorOperators.EQ, entryKeyVector1).trueCount();
-                int eqCount2 = keyVector1.compare(VectorOperators.EQ, entryKeyVector2).trueCount();
+                int eqCount2 = keyVector2.compare(VectorOperators.EQ, entryKeyVector2).trueCount();
 
                 entryOffset1 = map.putKey(keyVector1, keyStartPtr1, keyLength1, entryOffset1, keySize1, eqCount1);
                 entryOffset2 = map.putKey(keyVector2, keyStartPtr2, keyLength2, entryOffset2, keySize2, eqCount2);
