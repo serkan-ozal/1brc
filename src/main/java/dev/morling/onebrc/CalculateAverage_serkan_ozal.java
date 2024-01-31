e0886291eaa0b693c38f34885103a30254361524
Serkan ÖZAL <sozal@catchpoint.com>
2024-02-01 00:30:08 +0300

    wip

--- src/main/java/dev/morling/onebrc/CalculateAverage_serkan_ozal.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_serkan_ozal.java
@@ -761,7 +761,7 @@ public class CalculateAverage_serkan_ozal {
                 // Check for hash collision (hashes are same, but keys are different).
                 // If there is no collision (both hashes and keys are equals), return current slot's offset.
                 // Otherwise, continue iterating until find an available slot.
-                ByteVector entryKeyVector = ByteVector.fromArray(BYTE_SPECIES, data, entryOffset + KEY_ARRAY_OFFSET));
+                ByteVector entryKeyVector = ByteVector.fromArray(BYTE_SPECIES, data, entryOffset + KEY_ARRAY_OFFSET);
                 int eqCount = keyVector.compare(VectorOperators.EQ, entryKeyVector).trueCount();
                 if (eqCount == keyLength) {
                     return entryOffset;
