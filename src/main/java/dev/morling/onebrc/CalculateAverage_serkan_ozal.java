b5afab573ac8af92799c7d8d9b31faa9b9e87448
Serkan ÖZAL <sozal@catchpoint.com>
2024-01-31 21:07:06 +0300

    wip

--- src/main/java/dev/morling/onebrc/CalculateAverage_serkan_ozal.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_serkan_ozal.java
@@ -469,14 +469,14 @@ public class CalculateAverage_serkan_ozal {
 
                 // Put keys and calculate entry offsets to put values
                 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-                int entryOffset1 = map.putKey(keyVector1, keyStartPtr1, keyLength1, entryIdx1);
-                int entryOffset2 = map.putKey(keyVector2, keyStartPtr2, keyLength2, entryIdx2);
-//                if (entryOffset1 == 0) {
-//                    entryOffset1 = map.putKey(keyVector1, keyStartPtr1, keyLength1, entryIdx1);
-//                }
-//                if (entryOffset2 == 0) {
-//                    entryOffset2 = map.putKey(keyVector2, keyStartPtr2, keyLength2, entryIdx2);
-//                }
+                int entryOffset1 = map.putKeyFast(keyVector1, keyStartPtr1, keyLength1, entryIdx1);
+                int entryOffset2 = map.putKeyFast(keyVector2, keyStartPtr2, keyLength2, entryIdx2);
+                if (entryOffset1 == 0) {
+                    entryOffset1 = map.putKey(keyVector1, keyStartPtr1, keyLength1, entryIdx1 + 1);
+                }
+                if (entryOffset2 == 0) {
+                    entryOffset2 = map.putKey(keyVector2, keyStartPtr2, keyLength2, entryIdx2 + 1);
+                }
                 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
                 // Extract values by parsing and put them into map
