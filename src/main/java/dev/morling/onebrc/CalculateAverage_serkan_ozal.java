23e38486ee951c79465c642f907986edcc754f1c
Serkan ÖZAL <sozal@catchpoint.com>
2024-01-31 22:54:38 +0300

    wip

--- src/main/java/dev/morling/onebrc/CalculateAverage_serkan_ozal.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_serkan_ozal.java
@@ -475,17 +475,17 @@ public class CalculateAverage_serkan_ozal {
                 int entryOffset1 = Unsafe.ARRAY_BYTE_BASE_OFFSET + entryIdx1;
                 int entryOffset2 = Unsafe.ARRAY_BYTE_BASE_OFFSET + entryIdx2;
 
-                entryOffset1 = map.putKeyFast(keyVector1, keyStartPtr1, keyLength1, entryOffset1);
-                entryOffset2 = map.putKeyFast(keyVector2, keyStartPtr2, keyLength2, entryOffset2);
-                if (entryOffset1 == 0) {
-                    entryOffset1 = map.putKey(keyVector1, keyStartPtr1, keyLength1, entryOffset1 + OpenMap.ENTRY_SIZE);
-                }
-                if (entryOffset2 == 0) {
-                    entryOffset2 = map.putKey(keyVector2, keyStartPtr2, keyLength2, entryOffset2 + OpenMap.ENTRY_SIZE);
-                }
+//                entryOffset1 = map.putKeyFast(keyVector1, keyStartPtr1, keyLength1, entryOffset1);
+//                entryOffset2 = map.putKeyFast(keyVector2, keyStartPtr2, keyLength2, entryOffset2);
+//                if (entryOffset1 == 0) {
+//                    entryOffset1 = map.putKey(keyVector1, keyStartPtr1, keyLength1, entryOffset1 + OpenMap.ENTRY_SIZE);
+//                }
+//                if (entryOffset2 == 0) {
+//                    entryOffset2 = map.putKey(keyVector2, keyStartPtr2, keyLength2, entryOffset2 + OpenMap.ENTRY_SIZE);
+//                }
 //
-//                entryOffset1 = map.putKey(keyVector1, keyStartPtr1, keyLength1, entryOffset1);
-//                entryOffset2 = map.putKey(keyVector2, keyStartPtr2, keyLength2, entryOffset2);
+                entryOffset1 = map.putKey(keyVector1, keyStartPtr1, keyLength1, entryOffset1);
+                entryOffset2 = map.putKey(keyVector2, keyStartPtr2, keyLength2, entryOffset2);
                 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
                 // Extract values by parsing and put them into map
